4cd268306e265532df86b5b6777712a86fed2680
Jona Ackerschott <jona.a99@gmail.com>
2025-09-22 23:37:48 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -2,45 +2,41 @@
     description = "A simple cli experiment submission tool compatible with slurm clusters";
 
     inputs =  {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-        crane.url = "github:ipetkov/crane";
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     };
 
-    outputs = { nixpkgs, crane, ... }:
+    outputs = { nixpkgs, ... }:
     let
         system = "x86_64-linux";
         pkgs = nixpkgs.legacyPackages.${system};
-        craneLib = crane.mkLib pkgs;
-        sparrowUnwrapped = craneLib.buildPackage {
+        sparrow = pkgs.rustPlatform.buildRustPackage {
             name = "sparrow";
-            src = ./.;
-            nativeBuildInputs = [
-                pkgs.makeWrapper
-                pkgs.pkg-config
-                pkgs.openssl
-                pkgs.openssl.dev
+            src = pkgs.fetchFromGitHub {
+                owner = "jackerschott";
+                repo = "sparrow";
+                rev = "cbc957662bec31cc5e2b1589fc55f5702a24cd81";
+                sha256 = "sha256-+O79fzIFRjwxsRqglXEJ+8XIt4Eb4dXAz+/vvxkCIN4=";
+            };
+            nativeBuildInputs = with pkgs; [
+                pkg-config
+                makeWrapper
             ];
-
-            #postFixup = ''
-            #    wrapProgram \
-            #        "$out/bin/sparrow" \
-            #        --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.rsync ]}
-            #'';
-            PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
-        };
-        # this extra step is needed to add rsync to the PATH of sparrow,
-        # since $out in craneLib.buildPackage is not the right path
-        sparrow = pkgs.symlinkJoin {
-            name = "sparrow";
-            paths = [ sparrowUnwrapped ];
-            buildInputs = [ pkgs.makeWrapper pkgs.rsync ];
-
-            postBuild = ''
+            buildInputs = with pkgs; [
+                openssl
+                rsync
+                fzf
+            ];
+            cargoHash = "sha256-2HaqD8bIF4OvHfmFw5BVdwODnPNwBriBeo7Rx11C4ds=";
+            postFixup = ''
                 wrapProgram \
                     "$out/bin/sparrow" \
-                    --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.rsync ]}
+                    --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.rsync pkgs.fzf ]}
             '';
-            meta.mainProgram = "sparrow";
+            meta = {
+                description = "A simple cli experiment submission tool compatible with slurm clusters";
+                homepage = "https://gitlab.cern.ch/jackersc/sparrow";
+                license = pkgs.lib.licenses.mit;
+            };
         };
     in {
         packages.${system}.default = sparrow;
